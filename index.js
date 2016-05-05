cc7b7a85a75f1a758e42e35940803d89a157ed92
Andy Bangs <andrew.yuta@gmail.com>
2016-05-04 18:04:30 -0700

    setup express server for api

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 'use strict';
 
+const express = require('express');
 const fetchList = require('./fetchList');
-const fs = require('fs');
 
-fetchList('http://chucks85th.com/')
-  .then((json) => {
-    fs.writeFile('./results.json', json);
-  });
+const app = express();
+const port = process.env.PORT || 8080;
+const router = express.Router();
+
+router.get('/beers', function(req, res) {
+    fetchList('http://chucks85th.com/')
+      .then((json) => {
+        res.send(json);
+      });
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Express server started on port:', port);
