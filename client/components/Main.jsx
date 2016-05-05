6bbb8b8e59d5e47fc6e28b15c4201159d83dcbcd
Andy Bangs <andrew.yuta@gmail.com>
2016-05-05 15:27:05 -0700

    add filters select element

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -2,12 +2,20 @@ import React, { PropTypes } from 'react';
 
 const propTypes = {
   source: PropTypes.string.isRequired,
+  filters: PropTypes.object.isRequired,
 };
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { data: [] };
+
+    this.state = {
+      data: [],
+      filter: this.props.filters.ALL,
+    };
+
+    this.filterItem = this.filterItem.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
   }
 
   componentDidMount() {
@@ -26,7 +34,30 @@ class Main extends React.Component {
       });
   }
 
+  filterItem(item) {
+    const { filters } = this.props;
+    const { filter } = this.state;
+
+    if (filter === filters.ALL) return true;
+    if (filter === item.classes[1]) return true;
+    if (filter === filters.OTHER && !item.classes[1]) return true;
+
+    return false;
+  }
+
+  handleSelect(event) {
+    this.setState({ filter: event.target.value });
+  }
+
   render() {
+    const { filters } = this.props;
+    const { data, filter } = this.state;
+    const filtersArr = Object.keys(filters).map(key => filters[key]);
+
+    function createOption(val) {
+      return <option key={val} value={val}>{val}</option>;
+    }
+
     function createItem(item) {
       return <li key={item.tap}>{item.tap} - {item.brewery} | {item.beer}</li>;
     }
@@ -34,7 +65,12 @@ class Main extends React.Component {
     return (
       <div>
         <h2>Tap List</h2>
-        <ul>{this.state.data.map(createItem)}</ul>
+        <select value={filter} onChange={this.handleSelect}>
+          {filtersArr.map(createOption)}
+        </select>
+        <ul>
+          {data.filter(this.filterItem).map(createItem)}
+        </ul>
       </div>
     );
   }
--- client/index.jsx
+++ client/index.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './components/Main';
 
+const filters = {
+  ALL: 'all',
+  IPA: 'ipa',
+  SOUR: 'sour',
+  STOUT: 'stout',
+  CIDER: 'cider',
+  NITRO: 'nitro',
+  OTHER: 'other',
+};
+
 ReactDOM.render(
-  <Main source="/api/beers" />,
+  <Main source="/api/beers" filters={filters} />,
   document.getElementById('app')
 );
