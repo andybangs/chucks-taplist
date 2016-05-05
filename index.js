32c2c962ee3dc124905eca3590e0b565277f1c85
Andy Bangs <andrew.yuta@gmail.com>
2016-05-05 09:35:13 -0700

    add catchall route for spa

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ const fetchList = require('./fetchList');
 
 const app = express();
 const port = process.env.PORT || 8080;
-const router = express.Router();
 
-router.get('/beers', function(req, res) {
+app.get('/api/beers', function(req, res) {
     fetchList('http://chucks85th.com/')
       .then((json) => {
         res.send(json);
       });
 });
 
-app.use('/api', router);
+app.get('*', function(req, res) {
+  res.sendfile('./index.html');
+});
 
 app.listen(port);
 console.log('Express server started on port:', port);
