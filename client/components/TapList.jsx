cc013849983ebbaf279f09c6e87cb26fac50668f
Andy Bangs <andrew.yuta@gmail.com>
2016-05-09 12:12:20 -0700

    convert order dropdown to button

--- client/components/TapList.jsx
+++ client/components/TapList.jsx
@@ -1,6 +1,8 @@
 import React, { PropTypes } from 'react';
 import { filters, orders, endpoints } from '../constants';
 
+const ordersArr = Object.keys(orders).map(key => orders[key]);
+
 const propTypes = {
   params: PropTypes.object.isRequired,
 };
@@ -12,16 +14,16 @@ class TapList extends React.Component {
     this.state = {
       data: [],
       filter: filters.ALL,
-      order: orders.TAP,
+      orderIndex: 0,
     };
 
     this.fetchList = this.fetchList.bind(this);
     this.filterItem = this.filterItem.bind(this);
     this.compareItems = this.compareItems.bind(this);
     this.handleFilterClick = this.handleFilterClick.bind(this);
-    this.createButton = this.createButton.bind(this);
-    this.handleOrderSelect = this.handleOrderSelect.bind(this);
+    this.handleOrderClick = this.handleOrderClick.bind(this);
     this.handleResetClick = this.handleResetClick.bind(this);
+    this.createButton = this.createButton.bind(this);
   }
 
   componentDidMount() {
@@ -59,7 +61,7 @@ class TapList extends React.Component {
   }
 
   compareItems(a, b) {
-    switch (this.state.order) {
+    switch (ordersArr[this.state.orderIndex]) {
       case orders.BREWERY:
         return a.brewery.toLowerCase() < b.brewery.toLowerCase() ? -1 : 1;
       case orders.PRICE:
@@ -75,12 +77,14 @@ class TapList extends React.Component {
     this.setState({ filter: event.target.value });
   }
 
-  handleOrderSelect(event) {
-    this.setState({ order: event.target.value });
+  handleOrderClick() {
+    const { orderIndex } = this.state;
+    const nextIndex = orderIndex < ordersArr.length - 1 ? orderIndex + 1 : 0;
+    this.setState({ orderIndex: nextIndex });
   }
 
   handleResetClick() {
-    this.setState({ filter: filters.ALL, order: orders.TAP });
+    this.setState({ filter: filters.ALL, orderIndex: 0 });
   }
 
   createButton(val) {
@@ -97,13 +101,8 @@ class TapList extends React.Component {
   }
 
   render() {
-    const { data, filter, order } = this.state;
+    const { data, orderIndex } = this.state;
     const filtersArr = Object.keys(filters).map(key => filters[key]);
-    const orderArr = Object.keys(orders).map(key => orders[key]);
-
-    function createOption(val) {
-      return <option key={val} value={val}>{val}</option>;
-    }
 
     function createItem(item) {
       return (
@@ -115,13 +114,11 @@ class TapList extends React.Component {
 
     return (
       <div>
-        <h3>{filter}</h3>
         {filtersArr.map(this.createButton)}
-        <label htmlFor="order">Sort by: </label>
-        <select name="order" value={order} onChange={this.handleOrderSelect}>
-          {orderArr.map(createOption)}
-        </select>
-        <button onClick={this.handleResetClick}>Reset</button>
+        <button className="order" onClick={this.handleOrderClick}>
+          {ordersArr[orderIndex]}
+        </button>
+        <button className="reset" onClick={this.handleResetClick}>reset</button>
         <ul>
           {data.filter(this.filterItem).sort(this.compareItems).map(createItem)}
         </ul>
